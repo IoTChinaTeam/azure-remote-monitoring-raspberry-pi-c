0eb5acbcada0367c10208bf2f3e52cbcf9d95577
CuiJian630 <v-jiacu@microsoft.com>
2017-04-19 16:29:24 +0800

    remove initatefirmware update method from twin

--- Basic/samples/remote_monitoring/remote_monitoring.c
+++ Basic/samples/remote_monitoring/remote_monitoring.c
@@ -13,11 +13,6 @@
 #include <ctype.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
@@ -41,8 +36,7 @@ static int Lock_fd;
 
 /*json of supported methods*/
 static char* supportedMethod = "{ \"LightBlink\": \"light blink\", \"ChangeLightStatus--LightStatusValue-int\""
-": \"Change light status, on and off\", \"InitiateFirmwareUpdate--FwPackageURI-string\": "
-"\"Updates device Firmware. Use parameter FwPackageURI to specifiy the URI of the firmware file\"}";
+": \"Change light status, on and off\" }";
 
 // Define the Model
 BEGIN_NAMESPACE(Contoso);
@@ -83,7 +77,6 @@ WITH_DESIRED_PROPERTY(uint8_t, TelemetryInterval, onDesiredTelemetryInterval),
 /* Direct methods implemented by the device */
 WITH_METHOD(LightBlink),
 WITH_METHOD(ChangeLightStatus, int, LightStatusValue),
-WITH_METHOD(InitiateFirmwareUpdate, ascii_char_ptr, FwPackageURI),
 
 /* Register direct methods with solution portal */
 WITH_REPORTED_PROPERTY(ascii_char_ptr_no_quotes, SupportedMethods)
@@ -106,205 +99,6 @@ METHODRETURN_HANDLE ChangeLightStatus(Thermostat* thermostat, int lightstatus)
 	printf("LED value\n %d", lightstatus);
 	digitalWrite(Grn_led_pin, lightstatus);
 	return MethodReturn_Create(201, "\"light status changed\"");
-}
-
-void WriteConfig()
-{
-	FILE* fp;
-
-	if (NULL == (fp = fopen("//home//pi//lastupdate", "w")))
-	{
-		printf("Failed to open lastupdate file to write\r\n");
-	}
-	else
-	{
-		printf("last update begin value: %s\r\n", lastUpdateBegin);
-		printf("last reboot begin value: %s\r\n", lastRebootBegin);
-		fprintf(fp, "%s\r\n%s", lastUpdateBegin, lastRebootBegin);
-		fclose(fp);
-	}
-}
-
-bool GetNumberFromString(const unsigned char* text, size_t size, int* pValue)
-{
-	const unsigned char* pStart = text;
-	for (; pStart < text + size; pStart++)
-	{
-		if (isdigit(*pStart))
-		{
-			break;
-		}
-	}
-
-	const unsigned char* pEnd = pStart + 1;
-	for (; pEnd <= text + size; pEnd++)
-	{
-		if (!isdigit(*pEnd))
-		{
-			break;
-		}
-	}
-
-	if (pStart >= text + size)
-	{
-		return false;
-	}
-
-	unsigned char buffer[16] = { 0 };
-	strncpy(buffer, pStart, pEnd - pStart);
-
-	*pValue = atoi(buffer);
-	return true;
-}
-
-char* FormatTime(time_t* time)
-{
-	static char buffer[128];
-
-	struct tm* p = gmtime(time);
-
-	strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", p);
-
-	return buffer;
-}
-
-//download file in Git hub repo via wget as example
-bool DownloadFile(ascii_char_ptr url)
-{
-	printf("Download url: %s\r\n", url);
-	char str[256];
-	sprintf(str, "wget %s", url);
-	return system(str) == 0;
-}
-
-void AllocAndVPrintf(unsigned char** buffer, size_t* size, const char* format, va_list argptr)
-{
-	*size = vsnprintf(NULL, 0, format, argptr);
-
-	*buffer = malloc(*size + 1);
-	vsprintf((char*)*buffer, format, argptr);
-}
-
-void UpdateReportedProperties(const char* format, ...)
-{
-	unsigned char* report;
-	size_t len;
-
-	va_list args;
-	va_start(args, format);
-	AllocAndVPrintf(&report, &len, format, args);
-	va_end(args);
-
-	if (IoTHubClient_SendReportedState(g_iotHubClientHandle, report, len, NULL, NULL) != IOTHUB_CLIENT_OK)
-	{
-		(void)printf("Failed to update reported properties: %.*s\r\n", len, report);
-	}
-	else
-	{
-		(void)printf("Succeeded in updating reported properties: %.*s\r\n", len, report);
-	}
-
-	free(report);
-}
-
-//unzip the target package to the specify folder, then move it the downloaded file
-void ApplyFirmware()
-{
-	system("unzip -o remote_monitoring.zip -d cmake/samples/remote_monitoring/");
-	system("sudo mv cmake/samples/remote_monitoring/firmwarereboot.sh .");
-	system("sudo chmod +x firmwarereboot.sh");
-	system("sed -i 's/\r//' firmwarereboot.sh");
-	system("sudo chmod +x cmake/samples/remote_monitoring/remote_monitoring");
-	system("rm -f remote_monitoring.zip");
-	system("sudo nohup sh ./firmwarereboot.sh > /tmp/reboot.txt &");
-}
-
-void* FirmwareUpdateThread(void* arg)
-{
-	time_t begin, end, stepBegin, stepEnd;
-	printf("Firmware thread start, download url: %s\r\n", (char*)arg);
-	ascii_char_ptr url = arg;
-
-	// Clear all reportes
-	UpdateReportedProperties("{ 'Method' : { 'UpdateFirmware': null } }");
-	time(&begin);
-	char * beginUpdate = FormatTime(&begin);
-	lastUpdateBegin = malloc(strlen(beginUpdate) + 1);
-	strcpy(lastUpdateBegin, beginUpdate);
-	UpdateReportedProperties(
-		"{ 'Method' : { 'UpdateFirmware': { 'Duration-s': 0, 'LastUpdate': '%s', 'Status': 'Running' } } }",
-		beginUpdate);
-
-	time(&stepBegin);
-	UpdateReportedProperties(
-		"{ 'Method' : { 'UpdateFirmware': { 'Download' : { 'Duration-s': 0, 'LastUpdate': '%s', 'Status': 'Running' } } } }",
-		FormatTime(&stepBegin));
-
-	time(&stepEnd);
-	//downloadfile
-	if (!DownloadFile(url))
-	{
-		UpdateReportedProperties(
-			"{ 'Method' : { 'UpdateFirmware': { 'Download' : { 'Duration-s': %u, 'LastUpdate': '%s', 'Status': 'Failed' } } } }",
-			stepEnd - stepBegin,
-			FormatTime(&stepEnd));
-
-		time(&end);
-		UpdateReportedProperties(
-			"{ 'Method' : { 'UpdateFirmware': { 'Duration-s': %u, 'LastUpdate': '%s', 'Status': 'Failed' } } }",
-			end - begin,
-			FormatTime(&end));
-		return NULL;
-	}
-
-	time(&stepEnd);
-
-	UpdateReportedProperties(
-		"{ 'Method' : { 'UpdateFirmware': { 'Download' : { 'Duration-s': %u, 'LastUpdate': '%s', 'Status': 'Complete' } } } }",
-		stepEnd - stepBegin,
-		FormatTime(&stepEnd));
-
-	time(&stepBegin);
-	UpdateReportedProperties(
-		"{ 'Method' : { 'UpdateFirmware': { 'Applied' : { 'Duration-s': 0, 'LastUpdate': '%s', 'Status': 'Running' } } } }",
-		FormatTime(&stepBegin));
-
-	printf("unlock file before apply new firmware\r\n");
-	close_lockfile(Lock_fd);
-
-	ApplyFirmware();
-
-	time(&stepEnd);
-	UpdateReportedProperties(
-		"{ 'Method' : { 'UpdateFirmware': { 'Applied' : { 'Duration-s': %u, 'LastUpdate': '%s', 'Status': 'Complete' } } } }",
-		stepEnd - stepBegin,
-		FormatTime(&stepEnd));
-
-	time(&stepBegin);
-	char * rebootBegin = FormatTime(&stepBegin);
-	UpdateReportedProperties(
-		"{ 'Method' : { 'UpdateFirmware': { 'Reboot' : { 'Duration-s': 0, 'LastUpdate': '%s', 'Status': 'Running' } } } }",
-		rebootBegin);
-
-	lastRebootBegin = malloc(strlen(rebootBegin) + 1);
-	strcpy(lastRebootBegin, rebootBegin);
-	WriteConfig();
-	free(arg);
-	exit(0);
-}
-
-METHODRETURN_HANDLE InitiateFirmwareUpdate(Thermostat* thermostat, ascii_char_ptr FwPackageURI)
-{
-	(void)(thermostat);
-
-	METHODRETURN_HANDLE result = MethodReturn_Create(201, "\"Initiating Firmware Update\"");
-	printf("Recieved firmware update request. Use package at: %s\r\n", FwPackageURI);
-	pthread_t tid;
-	ascii_char_ptr url = malloc(strlen(FwPackageURI) + 1);
-	strcpy(url, FwPackageURI);
-	printf("receive and strcpy url: %s\r\n", url);
-	pthread_create(&tid, NULL, &FirmwareUpdateThread, url);
-	return result;
 }
 
 METHODRETURN_HANDLE LightBlink(Thermostat* thermostat)
